346b48c5643787d0031f1c90827af1ee5e17d329
SimeonDee <adeyemi.adedoyin.simeon@gmail.com>
2025-06-10 21:03:13 +0100

    updated db config

--- src/config/db.py
+++ src/config/db.py
@@ -5,6 +5,12 @@ from sqlalchemy import create_engine, MetaData
 load_dotenv()
 
 # db connections for user management
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_USERNAME = os.getenv("DB_USERNAME")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+
 DB_CONN_STR = os.getenv("DB_CONN_STR")
 
 metadata = MetaData()
